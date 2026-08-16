6c7c16f3c8d7b8746f27540497c00fc02e5ea6f7
GGORG <me@ggorg.xyz>
2026-08-17 00:01:45 +0200

    added rbw init script

---
+++ .install-rbw.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+type rbw >/dev/null 2>&1 && exit
+type pacman >/dev/null 2>&1 || echo "This script is only for Arch Linux" && exit 1
+
+sudo pacman -S --needed --noconfirm rbw
+
+rbw config set email me@ggorg.xyz
+rbw config set base_url https://vaultwarden.ggorg.xyz
+rbw login
