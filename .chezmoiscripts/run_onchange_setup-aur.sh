0c3ada6ac84f0fb975614fc6257ced984bcb15ba
GGORG <me@ggorg.xyz>
2026-08-17 18:15:24 +0200

    added an aur setup script

---
+++ .chezmoiscripts/run_onchange_setup-aur.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+type pacman >/dev/null 2>&1 || { echo "This script is only for Arch Linux"; exit 1; }
+
+setup_chaotic() {
+  pacman -Qi chaotic-mirrorlist >/dev/null 2>&1 && exit
+
+  echo "* Setting up chaotic-aur..."
+  
+  sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+  sudo pacman-key --lsign-key 3056513887B78AEB
+
+  sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+  sudo pacman -U --needed --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+  cat >>/etc/pacman.conf <<EOF
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
+EOF
+}
+
+install_paru() {
+  type paru >/dev/null 2>&1 && exit
+
+  echo "* Installing paru..."
+  
+  sudo pacman -S --needed --noconfirm paru
+}
+
+setup_chaotic
+sudo pacman -Syu --noconfirm
+install_paru
--- .install-rbw.sh
+++ .install-rbw.sh
@@ -3,6 +3,8 @@
 type rbw >/dev/null 2>&1 && rbw config show >/dev/null 2>&1 && exit
 type pacman >/dev/null 2>&1 || { echo "This script is only for Arch Linux"; exit 1; }
 
+echo "* Installing rbw..."
+
 sudo pacman -S --needed --noconfirm rbw
 
 rbw config set email me@ggorg.xyz
